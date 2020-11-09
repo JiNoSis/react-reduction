30e0c5a727da84017b461eb3b04387ed2ae2ef49
kingdom1447 <thanachjuno@hotmail.com>
2020-11-09 16:44:20 +0700

    Update Sidebar.js

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -5,12 +5,14 @@ import React from 'react';
 import { 
   FaUserTimes 
 } from 'react-icons/fa';
-import { FiGitPullRequest } from 'react-icons/fi';
+import { FiGitPullRequest,FiChrome } from 'react-icons/fi';
 import {
   MdAccountCircle,
   MdSentimentSatisfied,
   MdViewList,
   MdPoll,
+  MdBusinessCenter,MdStorage,MdAssignmentLate,
+  MdGroup,
   MdLocalLibrary
 } from 'react-icons/md';
 
@@ -33,17 +35,35 @@ const sidebarBackground = {
 };
 
 
-const navItems1 = [
-  { to: '/tables', name: 'TableTutorial', exact: true, Icon: MdAccountCircle },
+const navItemsS = [
+  { to: '/news-board', name: 'DashBoard News', exact: true, Icon: MdAccountCircle },
   { to: '/profiles', name: 'profile', exact: false, Icon: MdAccountCircle },
   { to: '/prof-profiles', name:"Professors", exact: false, Icon: MdAccountCircle},
   { to: '/', name: 'enrollment', exact: false, Icon: MdLocalLibrary },
   { to: '/', name: 'course list', exact: false, Icon: MdViewList },
-  { to: '/forms', name: 'req-certificate', exact: false, Icon: FiGitPullRequest },
+  { to: '/request', name: 'req-certificate', exact: false, Icon: FiGitPullRequest },
   { to: '/gpa', name: 'gpa info', exact: false, Icon: MdPoll },
   { to: '/suggest', name: 'suggestion form', exact: false, Icon: MdSentimentSatisfied },
   { to: '/', name: 'logout', exact: false, Icon: FaUserTimes }
 ];
+
+const navItemsT = [
+  { to: '/prof-profiles', name: 'profile', exact: true, Icon: MdAccountCircle },
+  { to: '/', name: 'course list', exact: false, Icon: MdViewList },
+  { to: '/suggest', name: 'suggestion form', exact: false, Icon: MdSentimentSatisfied },
+  { to: '/', name: 'logout', exact: false, Icon: FaUserTimes }
+];
+
+const navItemsD = [
+  { to: '/login', name: 'Login', exact: true, Icon: MdAccountCircle },
+  { to: '/news-board', name: 'News-Board', exact: true, Icon: FiChrome },
+  { to: '/course-class', name: 'Course-Class', exact: false, Icon: MdLocalLibrary },
+  { to: '/stu-search', name: 'Student Timetable', exact: false, Icon: MdGroup },
+  { to: '/ins-search', name: 'Instructor Timetable', exact: false, Icon: MdBusinessCenter },
+  { to: '/aca-calender', name: 'Academic Calender', exact: false, Icon: MdStorage },
+  { to: '/faqs', name: 'FAQs', exact: false, Icon: MdAssignmentLate },
+];
+
 
 
 const bem = bn.create('sidebar');
@@ -53,6 +73,9 @@ class Sidebar extends React.Component {
     isOpenComponents: true,
     isOpenContents: true,
     isOpenPages: true,
+    sideBarD:true,
+    sideBarS:false,
+    sideBarT:false
   };
 
   handleClick = name => () => {
@@ -84,8 +107,11 @@ class Sidebar extends React.Component {
               </span>
             </SourceLink>
           </Navbar>
-          <Nav vertical>
-            {navItems1.map(({ to, name, exact, Icon }, index) => (
+
+          {(() => {
+          if (this.state.sideBarD) {
+          return <Nav vertical>
+            {navItemsD.map(({ to, name, exact, Icon }, index) => (
               <NavItem key={index} className={bem.e('nav-item')}>
                 <BSNavLink
                   id={`navItem-${name}-${index}`}
@@ -101,6 +127,52 @@ class Sidebar extends React.Component {
               </NavItem>
             ))}
           </Nav>
+            }
+          })()}
+
+          {(() => {
+          if (this.state.sideBarS) {
+          return <Nav vertical>
+            {navItemsS.map(({ to, name, exact, Icon }, index) => (
+              <NavItem key={index} className={bem.e('nav-item')}>
+                <BSNavLink
+                  id={`navItem-${name}-${index}`}
+                  className="text-uppercase"
+                  tag={NavLink}
+                  to={to}
+                  activeClassName="active"
+                  exact={exact}
+                >
+                  <Icon className={bem.e('nav-item-icon')} />
+                  <span className="">{name}</span>
+                </BSNavLink>
+              </NavItem>
+            ))}
+          </Nav>
+            }
+          })()}
+
+          {(() => {
+          if (this.state.sideBarT) {
+          return <Nav vertical>
+            {navItemsT.map(({ to, name, exact, Icon }, index) => (
+              <NavItem key={index} className={bem.e('nav-item')}>
+                <BSNavLink
+                  id={`navItem-${name}-${index}`}
+                  className="text-uppercase"
+                  tag={NavLink}
+                  to={to}
+                  activeClassName="active"
+                  exact={exact}
+                >
+                  <Icon className={bem.e('nav-item-icon')} />
+                  <span className="">{name}</span>
+                </BSNavLink>
+              </NavItem>
+            ))}
+          </Nav>
+            }
+          })()}
         </div>
       </aside>
     );
