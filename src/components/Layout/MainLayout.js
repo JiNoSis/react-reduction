7a88df75d82e8092ad5265b811173097d7371dfd
khanadech007 <31152797+khanadech007@users.noreply.github.com>
2020-11-15 20:53:45 +0700

    Update MainLayout.js

--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -31,7 +31,7 @@ class MainLayout extends React.Component {
 
       this.notificationSystem.addNotification({
         title: <MdImportantDevices />,
-        message: 'Welome to Reduction Admin!',
+        message: 'Welome to SIIT-Website!',
         level: 'info',
       });
     }, 1500);
@@ -44,7 +44,7 @@ class MainLayout extends React.Component {
       this.notificationSystem.addNotification({
         title: <MdLoyalty />,
         message:
-          'Reduction is carefully designed template powered by React and Bootstrap4!',
+          'This website is design exclusively for SIIT-Student',
         level: 'info',
       });
     }, 2500);
