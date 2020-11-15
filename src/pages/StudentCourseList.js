430978263f03c47b3c1a2b080881e058897989cc
Thanach Ungkhara <45899044+JiNoSis@users.noreply.github.com>
2020-11-15 19:09:07 +0700

    Update StudentCourseList.js

--- src/pages/StudentCourseList.js
+++ src/pages/StudentCourseList.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Card, CardBody, CardHeader, Table } from 'reactstrap';
 import firebase from '../firebase.js';
 
-
-var StdID =6000000001;
 var Mon1,M1_room;
 var Mon2,M2_room;
 var Tue1,T1_room;
@@ -30,14 +28,18 @@ class StudentCourseList extends React.Component{
   };
 
   componentDidMount() {
-    const CourseRef= firebase.database().ref('Std_Course').orderByChild('StdID').equalTo(StdID);
-    CourseRef.once('value', (snapshot) => {
-      console.log(snapshot.val());
-      let courseref1 = snapshot.val();
-      console.log(courseref1);
-      let newState=[];
-      for (let course in courseref1){
-          newState.push({
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          currentUser: user
+        });
+        const StdID = this.state.currentUser.email.slice(0,10);
+        const CourseRef= firebase.database().ref('Std_Course').orderByChild('StdID').equalTo(StdID);
+        CourseRef.once('value', (snapshot) => {
+          let courseref1 = snapshot.val();      
+          let newState=[];
+          for (let course in courseref1){
+            newState.push({
               course_code: courseref1[course].course_code,
               course_name: courseref1[course].course_name,
               schedule_date: courseref1[course].schedule_date,
@@ -49,12 +51,11 @@ class StudentCourseList extends React.Component{
               section: courseref1[course].section,
               room: courseref1[course].room
           });
-
-           this.setState({Course: newState});
-
+          this.setState({Course: newState});
         };
       });
-
+      }
+    })
 };
 
 
@@ -142,7 +143,6 @@ render() {
            <thead>
             <tr>
                 <th class="schedule">Day/Time</th>
-                <th class="schedule">8:00-9:00</th>
                 <th class="schedule">9:00-10:00</th>
                 <th class="schedule">10:00-11:00</th>
                 <th class="schedule">11:00-12:00</th>
@@ -150,70 +150,59 @@ render() {
                 <th class="schedule">13:00-14:00</th>
                 <th class="schedule">14:00-15:00</th>
                 <th class="schedule">15:00-16:00</th>
-                <th class="schedule">16:00-17:00</th>
              </tr>   
             </thead>
             <tbody>
                 <tr class="schedule">
                   <th align="center">Monday</th>   
-                  <td></td>
-                  <td class="schedule">{Mon1}<br></br>{M1_room} </td>
+                  <td class="schedule">{Mon1}<br></br>{M1_room}</td>
                   <td class="schedule">{Mon1}<br></br>{M1_room}</td>
                   <td class="schedule">{Mon1}<br></br>{M1_room}</td>
                   <td className="lunch"> Lunch</td>
                   <td class="schedule">{Mon2} <br></br> {M2_room}</td>
                   <td class="schedule">{Mon2} <br></br> {M2_room}</td>
                   <td class="schedule">{Mon2} <br></br> {M2_room}</td>
-                  <td></td>
                 </tr>
                 <tr class="schedule">
                   <th align="center">Tuesday</th>   
-                  <td class="schedule"></td>
                   <td class="schedule">{Tue1}<br></br>{T1_room}</td>
                   <td class="schedule">{Tue1}<br></br>{T1_room}</td>
                   <td class="schedule">{Tue1}<br></br>{T1_room}</td>
                   <td className="lunch"> Lunch</td>
                   <td class="schedule">{Tue2}<br></br>{T2_room}</td>
                   <td class="schedule">{Tue2}<br></br>{T2_room}</td>
-                  <td>{Tue2}<br></br>{T2_room}</td>
-                  <td></td>
+                  <td class="schedule">{Tue2}<br></br>{T2_room}</td>
                 </tr>
                 <tr class="schedule">
                   <th align="center">Wednesday</th>   
-                  <td></td>
-                  <td>{Wed1}<br></br>{W1_room}</td>
-                  <td>{Wed1}<br></br>{W1_room}</td>
-                  <td>{Wed1}<br></br>{W1_room}</td>
+                  <td class="schedule">{Wed1}<br></br>{W1_room}</td>
+                  <td class="schedule">{Wed1}<br></br>{W1_room}</td>
+                  <td class="schedule">{Wed1}<br></br>{W1_room}</td>
                   <td className="lunch"> Lunch</td>
-                  <td>{Wed2}<br></br>{W2_room}</td>
-                  <td>{Wed2}<br></br>{W2_room}</td>
-                  <td>{Wed2}<br></br>{W2_room}</td>
-                  <td></td>
+                  <td class="schedule">{Wed2}<br></br>{W2_room}</td>
+                  <td class="schedule">{Wed2}<br></br>{W2_room}</td>
+                  <td class="schedule">{Wed2}<br></br>{W2_room}</td>
                 </tr>
                 <tr class="schedule">
                   <th align="center">Thursday</th>   
-                  <td></td>
-                  <td>{Thu1}<br></br>{Th1_room}</td>
-                  <td>{Thu1}<br></br>{Th1_room}</td>
-                  <td>{Thu1}<br></br>{Th1_room}</td>
+                  <td class="schedule">{Thu1}<br></br>{Th1_room}</td>
+                  <td class="schedule">{Thu1}<br></br>{Th1_room}</td>
+                  <td class="schedule">{Thu1}<br></br>{Th1_room}</td>
                   <td className="lunch"> Lunch</td>
-                  <td>{Thu2}<br></br>{Th2_room}</td>
-                  <td>{Thu2}<br></br>{Th2_room}</td>
-                  <td>{Thu2}<br></br>{Th2_room}</td>
-                  <td></td>
+                  <td class="schedule">{Thu2}<br></br>{Th2_room}</td>
+                  <td class="schedule">{Thu2}<br></br>{Th2_room}</td>
+                  <td class="schedule">{Thu2}<br></br>{Th2_room}</td>
                 </tr>
 
                 <tr class="schedule">
                   <th align="center">Friday</th>   
-                  <td></td>
-                  <td>{Fri1}<br></br>{F1_room}</td>
-                  <td>{Fri1}<br></br>{F1_room}</td>
-                  <td>{Fri1}<br></br>{F1_room}</td>
+                  <td class="schedule">{Fri1}<br></br>{F1_room}</td>
+                  <td class="schedule">{Fri1}<br></br>{F1_room}</td>
+                  <td class="schedule">{Fri1}<br></br>{F1_room}</td>
                   <td className="lunch"> Lunch</td>
-                  <td>{Fri2}<br></br>{F2_room}</td>
-                  <td>{Fri2}<br></br>{F2_room}</td>
-                  <td>{Fri2}<br></br>{F2_room}</td>
-                  <td></td>
+                  <td class="schedule">{Fri2}<br></br>{F2_room}</td>
+                  <td class="schedule">{Fri2}<br></br>{F2_room}</td>
+                  <td class="schedule">{Fri2}<br></br>{F2_room}</td>
                 </tr>
 
             </tbody>  
