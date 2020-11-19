63fffae78d15d03b9c25f75112e4760a651c8486
Thanach Ungkhara <45899044+JiNoSis@users.noreply.github.com>
2020-11-19 16:17:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,28 +8,12 @@ import componentQueries from 'react-component-queries';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './styles/reduction.scss';
 
-
-
-const AlertPage = React.lazy(() => import('pages/AlertPage'));
 const AuthModalPage = React.lazy(() => import('pages/AuthModalPage'));
-const BadgePage = React.lazy(() => import('pages/BadgePage'));
-const ButtonGroupPage = React.lazy(() => import('pages/ButtonGroupPage'));
-const ButtonPage = React.lazy(() => import('pages/ButtonPage'));
-const CardPage = React.lazy(() => import('pages/CardPage'));
-const ChartPage = React.lazy(() => import('pages/ChartPage'));
-const DropdownPage = React.lazy(() => import('pages/DropdownPage'));
-const FormPage = React.lazy(() => import('pages/FormPage'));
-const InputGroupPage = React.lazy(() => import('pages/InputGroupPage'));
-const ModalPage = React.lazy(() => import('pages/ModalPage'));
-const ProgressPage = React.lazy(() => import('pages/ProgressPage'));
-const TablePage = React.lazy(() => import('pages/TablePage'));
-const TypographyPage = React.lazy(() => import('pages/TypographyPage'));
-const WidgetPage = React.lazy(() => import('pages/WidgetPage'));
 const SuggestPage = React.lazy(() => import('pages/SuggestPage'));
-const CourseSearchPage = React.lazy(() => import('pages/CourseSearchPage'));
+//const CourseSearchPage = React.lazy(() => import('pages/CourseSearchPage'));
 const RequestPage = React.lazy(() => import('pages/RequestPage'));
 const StuSearchPage = React.lazy(() => import('pages/StuSearchPage'));
-const InsSearchPage = React.lazy(() => import('pages/InsSearchPage'));
+//const InsSearchPage = React.lazy(() => import('pages/InsSearchPage'));
 const NewsBoardPage = React.lazy(() => import('pages/NewsBoardPage'));
 const FAQsPage = React.lazy(() => import('pages/FAQsPage'));
 const AcaCalenderPage = React.lazy(() => import('pages/AcaCalenderPage'));
@@ -70,25 +54,9 @@ class App extends React.Component {
               <React.Suspense fallback={<PageSpinner />}>
                 <Route exact path="/" component={NewsBoardPage} />
                 <Route exact path="/login-modal" component={AuthModalPage} />
-                <Route exact path="/buttons" component={ButtonPage} />
-                <Route exact path="/cards" component={CardPage} />
-                <Route exact path="/widgets" component={WidgetPage} />
-                <Route exact path="/typography" component={TypographyPage} />
-                <Route exact path="/alerts" component={AlertPage} />
-                <Route exact path="/tables" component={TablePage} />
-                <Route exact path="/badges" component={BadgePage} />
-                <Route exact path="/button-groups" component={ButtonGroupPage} />
-                <Route exact path="/dropdowns" component={DropdownPage} />
-                <Route exact path="/progress" component={ProgressPage} />
-                <Route exact path="/modals" component={ModalPage} />
-                <Route exact path="/forms" component={FormPage} />
                 <Route exact path="/request" component={RequestPage} />
                 <Route exact path="/suggest" component={SuggestPage} />
-                <Route exact path="/input-groups" component={InputGroupPage} />
-                <Route exact path="/charts" component={ChartPage} />
-                <Route exact path="/course-class" component={CourseSearchPage}/>
                 <Route exact path="/stu-search" component={StuSearchPage}/>
-                <Route exact path="/ins-search" component={InsSearchPage}/>
                 <Route exact path="/news-board" component={NewsBoardPage}/>
                 <Route exact path="/faqs" component={FAQsPage}/>
                 <Route exact path="/aca-calender" component={AcaCalenderPage}/>
